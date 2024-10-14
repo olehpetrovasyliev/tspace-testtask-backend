a864c0402f6fe4f3cefd7184b818719c175af962
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-10-14 05:21:40 +0300

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
